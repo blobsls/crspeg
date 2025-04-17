c8cf5595abbde57d9e08881c2c5ef8e71ab14666
blob <166371237+blobsls@users.noreply.github.com>
2025-04-17 00:49:49 -0400

    Create reduxfile.js

---
+++ crspeg/crspeg.js/reduxfile.js
@@ -0,0 +1,14 @@
+const rf={
+  File: undefined,
+  Initiator: this,
+
+  // Load Redux
+  redux: "http://j-file-Msh12.com/#/Assets/redux/3821/412/%P_DEF%/redux.min.js",
+  thismodule: "http://j-file-Msh12.com/#/Assets/redux/3821/684/4/node/reduxfile.extra.js",
+  config: "http://jfile.com/partners/files/@redux/bin/def/sp-272b-278sn-2s26-IPRegulatedStorage%xinencode.php%_%access.spec.js%+&idn.server.db/config.re.js",
+
+  // jfile IDN (Identification Network)
+  id: "http://jfile.com/privacy/%dbase%_#_xcode+r+cli/eternal/locals/cli/identification.user.js",
+  network: "http://j-file-Msh12.com/#/%locked%_#_verification={@status}_#_identify_IPRegulatedStorage%xinencode.php%_%access.spec.js%/network/online/this.js", // online
+  network_offline: "http://j-file-Msh12.com/#/%locked%_#_verification={@status}_#_identify_IPRegulatedStorage%xinencode.php%_%access.spec.js%/network/offline/this.js" // offline
+}
